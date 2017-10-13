2f397c611560ec5600de2ae52c0a4ae060e41de5
geradrum <gera.96@icloud.com>
2017-10-13 17:44:05 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+    /**
+     * Renders the App component.
+     */
+
+    render() {
+        return (
+            <div>My react app!</div>
+        );
+    }
+
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+const VENDOR_LIBS = [
+    // Vendor libraries
+    'react',
+    'react-dom',
+    'react-router-dom'
+];
+
+const config = {
+    entry: {
+        main: './src/index.js',
+        vendor: VENDOR_LIBS
+      },
+    output: {
+        path: path.join(__dirname, './build'),
+        filename: '[name].[hash].js'
+    },
+    module: {
+        rules: [
+          {
+            exclude: /node_modules/,
+            test: /\.(js|jsx)$/,
+            use: 'babel-loader'
+          },
+          {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: [
+                { loader: 'css-loader' },
+                { loader: 'postcss-loader' },
+                { loader: 'sass-loader' }
+              ]
+            })
+          },
+          {
+            test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*$|$)/,
+            use: 'file-loader'
+          }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['main', 'vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new ExtractTextPlugin('styles.css'),
+        new FaviconsWebpackPlugin({
+            logo: path.join(__dirname, './favicon.png'),
+            prefix: '[name].[hash]',
+            emitStats: false,
+            inject: true,
+            icons: {
+                android: false,
+                appleIcon: false,
+                appleStartup: false,
+                coast: false,
+                favicons: true,
+                firefox: false,
+                opengraph: false,
+                twitter: false,
+                yandex: false,
+                windows: false
+            }
+        })
+    ],
+    devServer: {
+        historyApiFallback: true
+    }
+};
+
+if(process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
+
+module.exports = config;
